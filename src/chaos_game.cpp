1bc8454d10823a7e94977f4d1f5a9b15bd27f965
medattia <42179315+medattia@users.noreply.github.com>
2020-06-11 10:24:09 +0100

    cv namespace, optimized pixel acces
    plotting the dots in real time in window

--- src/chaos_game.cpp
+++ src/chaos_game.cpp
@@ -15,45 +15,34 @@ struct Triangle
      Point2d c;
 };
 
-
-Point2d center( Point2d* a,  Point2d* b)
+Point2d center(Point2d* a, Point2d* b)
 {
     Point2d center_pt;
-    center_pt.x=(a->x+b->x)/2;
-    center_pt.y=(a->y+b->y)/2;
+    center_pt=(*a+*b)/2.;
     return center_pt;
 };
+
 int main()
 {
     int nbr;
     int max_iter=50000;
     struct Triangle trgl;
-    Point2d current_pt;
-    Point2d pt_1, pt_2, pt_3;
+    Point2d current_pt(4.,4.);
+    Point2d pt_1(0.,0.), pt_2(4.,0.), pt_3(2.,2.);
     Point2d pts[max_iter];
-    
-	cv::Mat img(800,1000,CV_8U, cv::Scalar(255));;
-        
-    pt_1.x = 0.;
-    pt_1.y = 0.;
-    pt_2.x = 4.;
-    pt_2.y = 0.; 
-    pt_3.x = 2.;
-    pt_3.y = 2.;
+	Point2i int_pt;
+	
+	Mat img(800,1000,CV_8U, Scalar(255));;
     
     trgl.a=pt_1;
     trgl.b=pt_2;
     trgl.c=pt_3;
-    
-    current_pt.x=4.;
-    current_pt.y=4.;
-    int x_int, y_int;
+
     for(int i=0;i<max_iter;i++)
     {
         pts[i]=current_pt;
-        x_int=(int) (current_pt.x*200);
-		y_int=(int) (current_pt.y*200);
-        img.at<uchar>(y_int,x_int) = 0;
+		int_pt=(cv::Point_<int>) (current_pt*200);
+        img.at<uchar>(int_pt) = 0;
         nbr = rand() % 3 + 1;
         switch (nbr)
         {
@@ -68,11 +57,11 @@ int main()
             break;
         }
         //printf("pts[%d]:\n x=%g,\n y=%g. \n",i,current_pt.x,current_pt.y);
+	   	namedWindow(window_title, WINDOW_AUTOSIZE);
+		imshow(window_title, img);
+		cv::waitKey(1);	
     }
-    
-    cv::namedWindow(window_title, cv::WINDOW_AUTOSIZE);
-	cv::imshow(window_title, img);
-	cv::waitKey(0);	
+
 	cv::destroyAllWindows();
     return 0;
 }
