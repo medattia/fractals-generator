5e28ab8640a158e89dcc8450f262194c16785ea4
medattia <42179315+medattia@users.noreply.github.com>
2020-06-11 09:40:01 +0100

    fully functional chaos game
    plot the Sierpenski Gasket on 1000x800 image

--- src/chaos_game.cpp
+++ src/chaos_game.cpp
@@ -1,10 +1,22 @@
-// self explanatory
+// demonstrates the 
 #include <iostream>
+/// headers for openCV
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+///macros
+#define window_title "Iteration "
+
+//using namespace cv;
+using namespace std;
 
 struct Point
 {
-    int x;
-    int y;  
+    double x;
+    double y;  
 };
 
 
@@ -18,52 +30,63 @@ struct Triangle
 
 struct Point center(struct Point* a, struct Point* b)
 {
-    struct Point res;
-    res.x=a->x+b->x;
-    res.y=a->y+b->y;
-    return res;
+    struct Point center_pt;
+    center_pt.x=(a->x+b->x)/2;
+    center_pt.y=(a->y+b->y)/2;
+    return center_pt;
 };
 int main()
 {
     int nbr;
-    int max_iter=100;
-    struct Triangle t;
-    struct Point current;
-    struct Point a1, a2, a3;
+    int max_iter=500000;
+    struct Triangle trgl;
+    struct Point current_pt;
+    struct Point pt_1, pt_2, pt_3;
     struct Point pts[max_iter];
     
-    a1.x =-1;
-    a1.y = 0;
-    a2.x = 0;
-    a2.y = 2; 
-    a3.x = 1;
-    a3.y = 0;
+        cv::Mat img(800,1000,CV_8U, cv::Scalar(255));;
+        
+    pt_1.x = 0.;
+    pt_1.y = 0.;
+    pt_2.x = 4.;
+    pt_2.y = 0.; 
+    pt_3.x = 2.;
+    pt_3.y = 2.;
     
-    t.a=a1;
-    t.b=a2;
-    t.c=a3;
+    trgl.a=pt_1;
+    trgl.b=pt_2;
+    trgl.c=pt_3;
     
-    current.x=0;
-    current.y=0;
-    
+    current_pt.x=4.;
+    current_pt.y=4.;
+    int x_int, y_int;
     for(int i=0;i<max_iter;i++)
     {
-        pts[i]=current;
+        pts[i]=current_pt;
+        x_int=(int) (current_pt.x*200);
+		y_int=(int) (current_pt.y*200);
+        img.at<uchar>(y_int,x_int) = 0;
+        //printf("The current point is x=%g,y=%g \n ",current_pt.x,current_pt.y);
         nbr = rand() % 3 + 1;
+        //printf("The dice roll yields %d \n ",nbr);
         switch (nbr)
         {
             case 1:
-                current=center(&current,&a1);
+                current_pt=center(&current_pt,&pt_1);
             break;
             case 2:
-                current=center(&current,&a2);
+                current_pt=center(&current_pt,&pt_2);
             break;
             case 3:
-                current=center(&current,&a3);
+                current_pt=center(&current_pt,&pt_3);
             break;
         }
-        printf("pts[%d]:\n x=%d,\n y=%d. \n",i,current.x,current.y);
+        //printf("pts[%d]:\n x=%g,\n y=%g. \n",i,current_pt.x,current_pt.y);
     }
     
+    cv::namedWindow(window_title, cv::WINDOW_AUTOSIZE);
+	cv::imshow(window_title, img);
+	cv::waitKey(0);	
+	cv::destroyAllWindows();
     return 0;
 }
