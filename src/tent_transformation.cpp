b90d8f15e81978b4e0e4325f823e5a8eaab3385b
Mohamed Attia <mohamedattia237@gmail.com>
2020-06-26 22:17:49 +0100

    added koch curve and added a second tent trasformation

--- src/tent_transformation.cpp
+++ src/tent_transformation.cpp
@@ -2,17 +2,34 @@
 // demonstrates a onestep machine based on a combined formula
 
 #include <iostream>
+#include <cmath>
+double tent_frac(double &input)
+{
+	double frac, intpart, output;
+	frac = modf (input , &intpart);
+	if (frac <=0.5) output=2*frac;
+	else output=2*(1-frac);
+	return output;
+}
 
+tent(int &input)
+{
+    while (input>1)
+    {
+        if(input%2 == 0) input=input/2;
+        else input=3*input+1;
+        std::cout<<input<<std::endl;
+    }
+}
 int main()
 {
-    int A,i;
+    int A;
+    double x;
     printf("Arbitrary positive integer \n");
     scanf("%d",&A);
-    while (A>1)
-    {
-        if(A%2 == 0) A=A/2;
-        else A=3*A+1;
-        std::cout<<A<<std::endl;
-    }
+    tent(A);
+    x=0.142857143;
+    std::cout<< tent_frac(x) <<std::endl;
+    
     return 0;
 }
